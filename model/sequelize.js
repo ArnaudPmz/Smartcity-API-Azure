baa1991f8248acc04fbd2f896b5adf5bf6efaee4
Arnaud <20701373+ArnaudPmz@users.noreply.github.com>
2020-12-17 18:55:00 +0100

    Update sequelize.js

--- model/sequelize.js
+++ model/sequelize.js
@@ -5,7 +5,7 @@ const { Sequelize, Model } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
+    dialect: 'mariadb',
     dialectOptions: {
         encrypt: true
     },
